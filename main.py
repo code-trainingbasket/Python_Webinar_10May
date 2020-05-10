5718eab1962f066c8eb763f1b034caa29d6277b6
TrainingBasket <sourcecode@trainingbasket.in>
2020-05-10 18:12:26 +0530

    Added a basic api

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import flask
 
-from flask import render_template
+from flask import render_template,jsonify,request
 
 app = flask.Flask(__name__)
 
@@ -14,6 +14,25 @@ def home():
 def blogs():
 	all_blogs = ['Food','Clothes','Computers']
 	return render_template('blogs.html',all_blogs = all_blogs)
+
+
+@app.route('/api/v1/resources/checkprime',methods =['GET'])
+def check_prime_api():
+
+	if 'num' in request.args:
+		n = int(request.args['num'])
+	else:
+		return "No Number (num) provided "
+	prime = False
+	for i in range(2,n//2 +1):
+		if n% i == 0:
+			break
+	else:
+		prime = True
+
+	result = {'Number':n, "Is Prime":prime}
+
+	return jsonify(result)
 
 
 if __name__ =='__main__':
